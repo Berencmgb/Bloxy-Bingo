194b6bd49165d61cec87a1d0935e2aeb428e5fc6
berencmgb <BerenCMGB@gmail.com>
2021-07-04 14:04:09 +0100

    Auto clicking sequence has been developed. There are minor adjustments we could make to improve this system. Pixel checking to see if the number has already been pressed or not could really speed up the system.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import numpy as np
 import time
 from PIL import ImageGrab
-import pydirectinput as input
+import pydirectinput as directInput
+
+cardY = np.array([1230, 1265, 1300, 1335, 1370])
 
 card1X = np.array([710, 745, 780, 815, 850])
-card1Y = np.array([1230, 1265, 1300, 1335, 1370])
+card2X = np.array([910, 945, 980, 1015, 1050])
+card3X = np.array([1110, 1145, 1180, 1215, 1250])
+card4X = np.array([1310, 1345, 1380, 1415, 1450])
+card5X = np.array([1510, 1545, 1580, 1615, 1650])
+card6X = np.array([1710, 1745, 1780, 1815, 1850])
+
+BingoPos = 1280, 1110
 
 
 def CalculateTime(duration):
@@ -17,8 +25,37 @@ def CalculateTime(duration):
         print(duration)
         time.sleep(1)
 
+# Need to make a GUI to select game version. Is it American bingo? Arcade?
+
+
+def TryBingo():
+    directInput.PAUSE = 0.006
+    directInput.click(BingoPos[0], BingoPos[1])
+
 
 while True:
     for x in card1X:
-        for y in card1Y:
-            input.click(x,y)
+        for y in cardY:
+            directInput.PAUSE = 0.006
+            directInput.click(x,y)
+    for x in card2X:
+        for y in cardY:
+            directInput.PAUSE = 0.006
+            directInput.click(x,y)
+    for x in card3X:
+        for y in cardY:
+            directInput.PAUSE = 0.006
+            directInput.click(x,y)
+    for x in card4X:
+        for y in cardY:
+            directInput.PAUSE = 0.006
+            directInput.click(x,y)
+    for x in card5X:
+        for y in cardY:
+            directInput.PAUSE = 0.006
+            directInput.click(x,y)
+    for x in card6X:
+        for y in cardY:
+            directInput.PAUSE = 0.006
+            directInput.click(x,y)
+    TryBingo()
